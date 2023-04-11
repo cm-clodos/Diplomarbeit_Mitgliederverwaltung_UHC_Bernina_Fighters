36638d41a3dfc7a871d301999eb5cb20062ca60e
clodos <clodos_m92@hotmail.com>
2023-04-11 15:36:17 +0200

    Vue Client und Grundstruktur erstellt

---
+++ client/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+
+    <div class="app-content">
+
+
+
+      <router-view :key="$route.fullPath" />
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+
+</script>
+
+<style lang="scss">
+  @import "@/assets/styles/app.scss";
+</style>
