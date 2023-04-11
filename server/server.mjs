7012594d0a6186b8b83fd1849117e50abef75a90
clodos <clodos_m92@hotmail.com>
2023-04-11 14:57:34 +0200

    initialer commit für backend

---
+++ server/server.mjs
@@ -0,0 +1,37 @@
+import express from 'express';
+import cors from 'cors';
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+const port = process.env.SERVER_PORT || 3000;
+const hostname = process.env.SERVER_HOSTNAME || 'localhost';
+
+// Whitelist definieren für zugriff später nur client-url
+let corsOptions = {
+    origin: "*",
+    credentials: true,
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+// Für Built in Middleware für Formulardaten
+app.use(express.urlencoded({extended: true}));
+
+app.get('/', function (req, res) {
+    res.send('Hello World!')
+})
+
+app.use((req, res) => {
+    res.status(404).send('Route not found! Check your URL!');
+});
+
+app.listen(port, hostname,() => {
+    console.log(`Server running at http://${hostname}:${port}/`)
+    console.log(`Server started on port ${port}`);
+});
+
+export {app};
