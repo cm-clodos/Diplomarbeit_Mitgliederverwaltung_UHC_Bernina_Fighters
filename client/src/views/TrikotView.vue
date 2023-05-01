efc71798bada1d19e0047db85ed4047b495bd59e
clodos <clodos_m92@hotmail.com>
2023-05-01 11:30:17 +0200

    Trikot Übersichtstabelle erstellt und Layout an kleine Geräte angepasst

--- client/src/views/TrikotView.vue
+++ client/src/views/TrikotView.vue
@@ -1,10 +1,148 @@
 <template>
-
+  <main class="trikot">
+    <h1>Übersicht Trikots</h1>
+    <div class="container">
+      <div class="card">
+        <div class="card-header">
+          <h4>Trikots
+            <RouterLink to="/trikots/new" class="btn btn-primary float-end">Trikot hinzufügen</RouterLink>
+          </h4>
+        </div>
+        <div class="card-body">
+          <table class="table table-bordered">
+            <thead>
+            <tr>
+              <th>Mitglied</th>
+              <th @click=""> Nummer
+                <font-awesome-icon icon="sort"/>
+              </th>
+              <th @click=""> Trikotname
+                <font-awesome-icon icon="sort"/>
+              </th>
+              <th>Verfügbar
+                <font-awesome-icon icon="sort"/>
+              </th>
+              <th>Actions</th>
+            </tr>
+            </thead>
+            <tbody v-if="this.trikots.length > 0">
+            <tr v-for="(trikot, index) in trikots " :key="index">
+              <td data-cell="mitglied">
+                <select v-model="trikot.memberId">
+                  <option value="null">- Nicht zugewiesen -</option>
+                  <option v-for="member in members" :value="member.id">{{ renderMemberName(member) }}</option>
+                </select>
+              </td>
+              <td data-cell="nummer"> {{ trikot.number }}</td>
+              <td data-cell="trikotname"><input type="text" class="form-control border-transparent" v-model="trikot.name"></td>
+              <td data-cell="verfügbar"> {{ trikot.available }} <input type="checkbox" @change="toggleAvailable(trikot)"
+                                                 v-bind:checked="trikot.available ===1" :value="trikot.available"></td>
+              <td data-cell="actions">
+                <div class="actions-container">
+                <button type="button"
+                        @click="updateTrikot(trikot.number, trikot.memberId, trikot.available, trikot.name)"
+                        class="btn btn-success action-btn">
+                  <font-awesome-icon class="action-icon" icon="floppy-disk"/>
+                </button>
+                <button type="button" @click="deleteTrikot(trikot.number)" class="btn btn-danger action-btn">
+                  <font-awesome-icon class="action-icon" icon="trash-can"/>
+                </button>
+                </div>
+              </td>
+            </tr>
+            </tbody>
+            <tbody v-else>
+            <tr>
+              <td colspan="8" class="text-center">Daten werden geladen...</td>
+            </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
+import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
+import {useToast} from 'vue-toast-notification';
+import axios from "/src/api/axios.mjs";
 export default {
-  name: "TrikotView"
+  name: "TrikotView",
+  components: {
+    FontAwesomeIcon
+  },
+  data() {
+    return {
+      toast: useToast(),
+      trikots: [],
+      members: [],
+    }
+  },
+  mounted() {
+    this.getAllTrikots();
+    this.getAllMembers();
+  },
+  methods: {
+    getAllTrikots() {
+      axios.get('/trikots')
+          .then(res => {
+            this.trikots = res.data;
+          })
+          .catch(error => {
+            console.log(error)
+            this.toast.error('Fehler beim Laden der Trikots');
+          });
+    },
+    getAllMembers() {
+      axios.get('/members')
+          .then(res => {
+            this.members = res.data;
+          })
+          .catch(error => {
+            console.log(error)
+            this.toast.error('Fehler beim Laden der Mitglieder');
+          });
+    },
+    updateTrikot(trikotNumber, memberId, available, trikotName) {
+      axios.put('/trikots/' + trikotNumber, {
+        member_id: memberId,
+        available: available,
+        name: trikotName
+      })
+          .then(res => {
+            console.log(res.data)
+            return this.toast.success('Trikot erfolgreich aktualisiert');
+          })
+          .catch(error => {
+            console.log(error)
+            if (error.response.status === 400) {
+              return this.toast.error('Fehler beim Aktualisieren: Mitglied besitzt bereits ein Trikot');
+            }
+            this.toast.error('Fehler beim Aktualisieren des Trikots');
+          });
+    },
+    deleteTrikot(trikotNumber) {
+      axios.delete(`/trikots/${trikotNumber}`)
+          .then(res => {
+            console.log(res.data)
+            this.toast.success('Trikot erfolgreich gelöscht');
+            this.getAllTrikots();
+          })
+          .catch(error => {
+            console.log(error)
+            this.toast.error('Fehler beim Löschen des Trikots');
+          });
+    },
+
+    toggleAvailable(trikot) {
+      trikot.available = trikot.available === 1 ? 0 : 1;
+      console.log(trikot.available)
+    },
+    renderMemberName(member) {
+      return member.firstname && member.lastname ? member.firstname + ' ' + member.lastname : '';
+    },
+  }
 }
 </script>
 
