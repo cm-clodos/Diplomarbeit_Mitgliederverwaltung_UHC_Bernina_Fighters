9238358e03dc6cf9635a907d61284ac71a3d9fcc
clodos <clodos_m92@hotmail.com>
2023-05-01 11:11:06 +0200

    View mit einfacher Export Funktion für Mitgliederliste

--- client/src/views/MemberStatisticExportView.vue
+++ client/src/views/MemberStatisticExportView.vue
@@ -1,10 +1,34 @@
 <template>
-
+  <div class="statistics">
+    <h1>Mitglieder Statistik und Exporte</h1>
+    <div>
+      <form ref="downloadForm" action="http://localhost:3000/members/export/download" method="POST"  hidden>
+        <input type="hidden" name="_method" value="POST" />
+      </form>
+      <button class="btn btn-primary" @click="download">Export Mitgliederliste</button>
+    </div>
+  </div>
 </template>
 
 <script>
+import {useToast} from 'vue-toast-notification';
 export default {
-  name: "MemberStatisticExportView"
+  name: "MemberStatisticExportView",
+  components: {},
+  data() {
+    return {
+      toast: useToast(),
+    }
+  },
+  mounted() {
+
+  },
+  methods: {
+
+    download() {
+      this.$refs.downloadForm.submit();
+    },
+  }
 }
 </script>
 
