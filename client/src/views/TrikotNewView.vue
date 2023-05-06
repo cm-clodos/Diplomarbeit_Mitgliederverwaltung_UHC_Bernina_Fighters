22e6bc46798716e5e47d0e8faa115a0d4f420cac
clodos <clodos_m92@hotmail.com>
2023-05-06 11:52:28 +0200

    Formularvalidierung hinzugefügt für das Formular, um Trikots hinzuzufügen

--- client/src/views/TrikotNewView.vue
+++ client/src/views/TrikotNewView.vue
@@ -1,6 +1,7 @@
 <template>
   <main class="trikotNew">
     <div class="container mt-5">
+      <form ref="form" @submit.prevent="handleSubmit">
       <div class="card">
         <div class="card-header">
           <h4>Trikot hinzufügen</h4>
@@ -10,11 +11,17 @@
             <div class="col-md-6">
               <div class="mb-3">
                 <label for="trikotNumber" class="form-label">Trikotnummer</label>
-                <input type="number" class="form-control" id="trikotNumber " v-model="model.trikot.number" required>
+                <input type="number" class="form-control" id="trikotNumber " v-model="model.trikot.number">
+                <span v-if="v$.model.trikot.number.$error" :class="`${v$.model.trikot.number.$error ? 'error-message' : ''}`">
+              {{ v$.model.trikot.number.required.$message }}
+            </span>
               </div>
               <div class="mb-3">
                 <label for="trikotName" class="form-label">Trikotname</label>
-                <input type="text" class="form-control" id="trikotName" v-model="model.trikot.name" required>
+                <input type="text" class="form-control" id="trikotName" v-model="model.trikot.name" >
+                <span v-if="v$.model.trikot.name.$error" :class="`${v$.model.trikot.name.$error ? 'error-message' : ''}`">
+              {{ v$.model.trikot.name.required.$message }}
+            </span>
               </div>
               <div class="mb-3">
                 <label for="available" class="form-label">Verfügbar</label>
@@ -33,10 +40,11 @@
             </div>
           </div>
           <div class="mb-3">
-            <button type="button" @click="addTrikot" class="btn btn-primary">Trikot hinzufügen</button>
+            <button type="submit" class="btn btn-primary">Trikot hinzufügen</button>
           </div>
         </div>
       </div>
+      </form>
     </div>
   </main>
 </template>
@@ -44,7 +52,12 @@
 <script>
 import {useToast} from 'vue-toast-notification';
 import axios from "/src/api/axios.mjs";
+import {useVuelidate} from "@vuelidate/core";
+import {required, helpers} from "@vuelidate/validators";
 export default {
+  setup() {
+    return {v$: useVuelidate()}
+  },
   name: "TrikotNewView",
   data() {
     return {
@@ -58,6 +71,16 @@ export default {
           member_id: null
         }
       },
+    }
+  },
+  validations(){
+    return {
+      model: {
+        trikot: {
+          number: {required: helpers.withMessage('Trikotnummer ist erforderlich', required)},
+          name: {required: helpers.withMessage('Trikotname ist erforderlich', required)}
+        }
+      }
     }
   },
   mounted() {
@@ -88,13 +111,34 @@ export default {
           .catch(error => {
             console.log(error);
             if ([400, 500].includes(error.response.status)) {
-              this.toast.error(error.response.data.message);
-              console.log(error.response.data)
+              if (error.response.data.message) {
+                this.toast.error(error.response.data.message);
+              } else if (error.response.data.length > 0) {
+                error.response.data.forEach((errorObj) => {
+                  Object.values(errorObj).forEach((errorMessage) => {
+                    this.toast.error(String(errorMessage));
+                  });
+                });
+              }
+              console.log(error.response.data);
             } else {
               console.log("Unexpected error: " + error.response.status);
             }
           });
-    }
+    },
+    async handleSubmit() {
+      const valid = await this.v$.$validate();
+      if (valid) {
+        try {
+          this.addTrikot();
+        } catch (err) {
+          this.toast.error("Fehler beim übermitteln des Formulars!")
+          console.log(err)
+        }
+      } else {
+        this.toast.error("Bitte fülle die Felder korrekt aus!")
+      }
+    },
   }
 }
 </script>
