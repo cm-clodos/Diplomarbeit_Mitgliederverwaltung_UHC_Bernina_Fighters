41e2e6500b16ad89dac5045a4a4a16398324c3de
clodos <clodos_m92@hotmail.com>
2023-05-18 13:23:52 +0200

    Modulimport fehler behoben + Workflow angepasst

--- server/controller/memberController.mjs
+++ server/controller/memberController.mjs
@@ -1,4 +1,4 @@
-import MemberHelper from "../helper/memberHelper.mjs";
+import MemberHelper from "../helper/MemberHelper.mjs";
 import Member from "../model/member.mjs";
 import CreateResponse from "../model/CreateResponse.mjs";
 import ApiError from "../model/ApiError.mjs";
