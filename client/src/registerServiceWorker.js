43e16bf7e1e1d1951852cb0271bff8c90f82d456
clodos <clodos_m92@hotmail.com>
2023-06-21 12:44:40 +0200

    Kommentar entfernt, als Beispiel belassen

--- client/src/registerServiceWorker.js
+++ client/src/registerServiceWorker.js
@@ -1,6 +1,4 @@
 /* eslint-disable no-console */
-/*
-
 import { register } from 'register-service-worker'
 
 if (process.env.NODE_ENV === 'production') {
@@ -32,4 +30,4 @@ if (process.env.NODE_ENV === 'production') {
   })
 }
 
- */
+
